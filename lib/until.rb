d5d5a5e717fad1be5e4ff672ee45610d06057ac9
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 22:37:50 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -3,7 +3,7 @@ def using_until
   levitation_force = 6
   #levitation_force = 0
 
-  until levitation_force == 10 do
+  until levitation_force == 10
     puts "Wingardium Leviosa' until"
     levitation_force = levitation_force + 1
   end
