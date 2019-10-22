c9ece8ec6bfb7f8ce765b8490e8ca09b4ade00cd
generic-puppet-7755 <tasmith1990@gmail.com>
2019-10-22 22:41:31 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,7 +3,7 @@ def using_while
   levitation_force = 6
   #levitation_force = 0
   
-  while levitation_force <= 10 do
+  while levitation_force < 10
     puts "Wingardium Leviosa"
     levitation_force += 1
   end
